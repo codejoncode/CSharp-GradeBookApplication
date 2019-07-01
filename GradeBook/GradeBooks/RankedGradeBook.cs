001ba4fa3539fed7b8fe025c2e0dc540fcad6b82
Jonathan <jonathanjholloway@gmail.com>
2019-06-30 20:55:49 -0500

    tests for b c d f failed because I had greater than or equal to instead if less than or equal too.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,42 +13,40 @@ namespace GradeBook.GradeBooks
         }
         //override the GetLetterGrade method   returns char and accepts a double named "averageGrade"
         public override char GetLetterGrade(double averageGrade)
-        
         {
-
             if (Students.Count < 5)
             {
+                // requires  using System 
                 throw new InvalidOperationException("Ranked grading requries at least 5 students.");
             }
             //integer Math.Ceiling Students.Count multipled by the 20% cast to a int
-            var threshold = (int)Math.Ceiling(Students.Count * 0.2); // 20% of students 
+            int threshold = (int)Math.Ceiling(Students.Count * 0.2); // 20% of students 
             /*
              Equilavent to a for loop and count how many grades are higher or lower than the average grade. 
              first orders then selects average grade and not the object then makes it a list. 
              */
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
             //how many students before we go down a later grade subtract one because of zero base indexing
-            if (grades[threshold - 1] >= averageGrade)
-            {
+            if (grades[threshold - 1] <= averageGrade)
+
                 return 'A';
-            }
+
             //* 2  because it will be twice what we need to drop a letter grade minus one because of zero base indexing
             else if (grades[(threshold * 2) - 1] <= averageGrade)
-            {
+
                 return 'B';
-            }
+
             else if (grades[(threshold * 3) - 1] <= averageGrade)
-            {
+
                 return 'C';
-            }
+
             else if (grades[(threshold * 4) - 1] <= averageGrade)
-            {
+
                 return 'D';
-            }
+
             else
                 return 'F';
-
-
+           
         }
     }
 }
